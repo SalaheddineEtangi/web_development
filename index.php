87ff38a6298665a789a7b03ac2ccdf78c4451d1e
SalaheddineEtangi <mo.momen1438@gmail.com>
2021-01-23 01:19:12 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Description" content="Le site web de Imane Oumelk">
+    <meta name="Keywords" 
+          content="Imane Oumelk, IHE, EHTP, Stage Ingénieur, Ingénieur d'Etat, Classes Préparatoires, Sciences-Mathématiques A,CV">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="fontawesome-free-5.15.2-web/css/all.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" 
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>Imane Oumelk</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #0096c7;">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Acceuil</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="contact.php">Contactez moi</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="messages.php">Messages</a>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <input class="form-control me-2" type="Rechercher" placeholder="Rechercher" aria-label="Rechercher">
+              <button class="btn btn-outline-light" type="submit">Rechercher</button>
+            </form>
+          </div>
+        </div>
+    </nav>
+    <div class="body">
+        <div class="mainColumn">
+            <p>
+                <div style="float: left; margin-right: 10px">
+                    <img src="./images/imane_w_p.PNG" alt="Photo de profil WhatsApp de Imane Oumelk" width="106"/>
+                </div>
+                <h2 style="margin-bottom: 1px;">Imane Oumelk</h2>
+                <h3 style="color: #0096c7; margin-top: 1px;">Etudiante ingénieur en Ingénierie de l'Hydraulique et de l'Environnement, à la recherche d'un Stage Ingénieur</h3>
+            </p><br>
+            <p class="anotherSpecialText" style="border-left: solid 5px #0096c7; padding:10px;">
+                <em>Je suis Imane Oumelk, étudiante ingénieur en Ingénierie de l'Hydraulique et de l'Environnement (<strong>IHE</strong>), 
+                à l'Ecole Hassania des Travaux Publics (<strong>EHTP</strong>), 
+                à la recherche d'un Stage Ingénieur (<strong>SI</strong>) dans un bureau d'études 
+                d'une durée minimale de 6 semaines à partir du Juin 2021.</em>
+            </p>
+            <h3 class="specialText">Formations</h3>
+            <h4>Cycle Ingénieur d'Etat / <span class="specialText">Ecole Hassania des Travaux Publics (EHTP)</span> Casablanca</h4>
+            <h5  class="anotherSpecialText">Depuis Septembre 2019</h5>
+            <p>
+                Étudiante ingénieur en deuxième année Ingénierie de l'Hydraulique et de l'Environnement. 
+            </p>
+            <h4>Classes Préparatoires aux Grandes Ecoles / <span class="specialText">Centre CPGE Beni Mellal</span> Beni Mellal</h4>
+            <h5 class="anotherSpecialText">De Septembre 2017 à Mai 2019</h5>
+            <p>
+                Option : Mathématiques et Physiques (<strong>MP</strong>)
+            </p>
+            <h4>Baccalauréat / <span class="specialText">Lycée Moulay Rchid</span> Kasba-Tadla</h4>
+            <h5 class="anotherSpecialText">De Septembre 2016 à Juin 2017</h5>
+            <p>
+                <ul>
+                    <li>Option : Sciences-Mathématiques A</li>
+                    <li>Mention : Bien</li>
+                </ul>   
+            </p>
+        </div>
+        <div class="asideColumn">
+            <table width="600"  class="anotherSpecialText">
+                <tbody>
+                    <tr>
+                        <td><i class="fas fa-envelope"></i></td>
+                        <td>imaneoumelk2bac@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td><i class="fas fa-map-marker-alt"></i></td>
+                        <td>Kasba-Tadla, Beni Mellal, Maroc</td>
+                    </tr>
+                    <tr>
+                        <td><i class="far fa-calendar-alt"></i></td>
+                        <td>20 ans</td>
+                    </tr>
+                    <tr>
+                        <td><i class="fab fa-font-awesome-flag"></i></td>
+                        <td>Marocaine</td>
+                    </tr>
+                    <tr>
+                        <td><i class="fas fa-phone-alt"></i></td>
+                        <td>07-00-66-88-30</td>
+                    </tr>
+                    <tr>
+                        <td><i class="fas fa-users"></i></td>
+                        <td>Célibataire</td>
+                    </tr>
+                </tbody>
+            </table><br>
+            <h3 class="specialText">Langues</h3>
+            <strong>Arabe</strong><br><br>
+            <div class="progressionBar" style="width: 170px; float: left;"></div><div class="disProgressionBar" style="width: 30px; float: left;"></div><br><br>
+            <strong>Français</strong><br><br>
+            <div class="progressionBar" style="width: 150px; float: left;"></div><div class="disProgressionBar" style="width: 50px; float: left;"></div><br><br>
+            <strong>Anglais</strong><br><br>
+            <div class="progressionBar" style="width: 130px; float: left;"></div><div class="disProgressionBar" style="width: 70px; float: left;"></div><br><br>  
+            <h3 class="specialText">Centre d'interêt</h3>
+            <p>
+                <strong>Réseaux sociaux &nbsp;</strong>Mon compte sur <a href="https://www.facebook.com/imane.oumelk" target="_blank" class="lien">Facebook</a>
+            </p>
+            <p>
+                <strong>Lecture des CVs &nbsp;</strong>CV de mon petit ami <a href="files/CV_Salah-eddine _Etangi.pdf" target="_blank" class="lien">Salah-eddine Etangi</a>.
+            </p>
+            <p>
+                <strong>Regarder YouTube &nbsp;</strong><br/>
+                <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+                <div class="elfsight-app-98a02de9-f2c3-4776-8faf-d6147b799327"></div>
+            </p>
+            <p>
+                <strong>Musique &nbsp;</strong>C'est l'une de mes chansons préférées : <br/>
+                <audio controls>
+                    <source src="media/bntelguiran.mp4">
+                </audio>
+            </p>
+        </div>
+    </div>
+    <div class="footer">Copyrights 2022, Imane Oumelk web site</div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
