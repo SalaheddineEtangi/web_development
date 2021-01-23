6c3942aca32b04182f3df25688c04eec64b50450
Salah-eddine Etangi <48295072+SalaheddineEtangi@users.noreply.github.com>
2021-01-23 22:32:46 +0100

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,10 @@
+<<<<<<< HEAD
+function cacher(id){
+    document.getElementById(id).innerHTML = "";
+}
+function afficher(id){
+    document.getElementById(id).innerHTML = "<p class=\"anotherSpecialText\" style=\"border-left: solid 5px #0096c7; padding:10px;\"><em>Je suis Imane Oumelk, étudiante ingénieur en Ingénierie de l'Hydraulique et de l'Environnement (<strong>IHE</strong>), à l'Ecole Hassania des Travaux Publics (<strong>EHTP</strong>), à la recherche d'un Stage Ingénieur (<strong>SI</strong>) dans un bureau d'études d'une durée minimale de 6 semaines à partir du Juin 2021.</em></p>";
+}
+=======
+
+>>>>>>> 0fe7e632599480f4a208e59676577c0af42e3b04
